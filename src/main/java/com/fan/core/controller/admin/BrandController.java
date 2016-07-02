c1b22b7c15a347d05e16f60a324a72e1223154f3
fqc <337940626@qq.com>
2016-07-02 18:50:05 +0800

    增加商品模块 页面跳转

---
+++ src/main/java/com/fan/core/controller/admin/BrandController.java
@@ -0,0 +1,12 @@
+package com.fan.core.controller.admin;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by fqc on 2016/7/2.
+ */
+@Controller
+
+public class BrandController {
+
+}
---
+++ src/main/java/com/fan/core/controller/admin/FrameController.java
@@ -0,0 +1,30 @@
+package com.fan.core.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by fqc on 2016/7/2.
+ */
+@Controller
+@RequestMapping("admin/frame")
+public class FrameController {
+
+    @RequestMapping("product_main.do")
+    public String product_main() {
+        return "frame/product_main";
+    }
+
+    @RequestMapping("product_left.do")
+    public String product_left() {
+        return "frame/product_left";
+    }
+
+    @RequestMapping("product_right.do")
+    public String product_right() {
+        return "frame/product_right";
+    }
+
+
+
+}
