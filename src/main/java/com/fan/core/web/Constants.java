8bf0cfc7a1a65899c8c25e3991ba339dce947a4b
fqc <337940626@qq.com>
2016-07-03 09:13:48 +0800

    添加品牌实体

---
+++ src/main/java/com/fan/core/web/Constants.java
@@ -0,0 +1,13 @@
+package com.fan.core.web;
+/**
+ * 业务常量
+ * Created by fqc on 2016/07/03.
+ *
+ */
+public interface Constants {
+
+	/**
+	 * 图片服务器
+	 */
+	public static final String IMAGE_URL = "http://localhost:8088/image-web/";
+}
