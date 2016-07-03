336012093d928640d628db11ff0c1b9a1da67a64
fqc <337940626@qq.com>
2016-07-03 22:29:12 +0800

    移动了页面位置

--- src/main/java/com/fan/core/controller/admin/BrandController.java
+++ src/main/java/com/fan/core/controller/admin/BrandController.java
@@ -1,5 +1,6 @@
 package com.fan.core.controller.admin;
 
+import com.fan.core.bean.product.Brand;
 import com.fan.core.service.product.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("brand")
 public class BrandController {
 
+    @Autowired
+    private BrandService brandService;
     /*@Autowired
     private BrandService brandService;*/
 
@@ -33,4 +36,19 @@ public class BrandController {
     public String toAdd() {
         return "brand/add";
     }
+
+    @RequestMapping("add.do")
+    public String add(Brand brand) {
+        Brand brand2 = new Brand();
+        brand2.setName("testxx");
+        brand2.setDescription("testxx");
+        brand2.setImgUrl("urlxx");
+        brand2.setSort(1);
+        brand2.setIsDisplay(1);
+        brandService.addBrand(brand2);
+        return "redirect:brand/list.do";
+    }
+
+
+
 }
